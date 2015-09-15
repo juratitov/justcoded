200c9d424e66cb7d557b045f1223da82a0309d8c
juratitov <juratitov@gmail.com>
2015-09-15 11:24:04 +0300

    Add CRUD

--- config/web.php
+++ config/web.php
@@ -5,7 +5,17 @@ $params = require(__DIR__ . '/params.php');
 $config = [
     'id' => 'basic',
     'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'bootstrap' => ['log', 'gii'],
+    'defaultRoute' => 'backend/default',
+    'modules' => [
+        'gii' => ['class' => 'yii\gii\Module'],
+        'backend' => [
+            'class' => 'app\modules\backend\Module',
+        ],
+        'api' => [
+            'class' => 'app\modules\api\Module',
+        ],
+    ],
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
@@ -38,14 +48,14 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
+            'enableStrictParsing' => false,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
+
     ],
     'params' => $params,
 ];
